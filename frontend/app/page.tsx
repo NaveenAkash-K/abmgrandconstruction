947efbe91f37f087e0fb4ccf2cd1485f8e993eae
Naveen Akash <naveen.akash0904@gmail.com>
2025-12-22 01:31:51 +0530

    Removed client section related files

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@ import HeroSection from "@/components/client/sections/HeroSection";
 import AboutSection from "@/components/client/sections/AboutSection";
 import ServicesSection from "@/components/client/sections/ServicesSection";
 import ProjectsSection from "@/components/client/sections/ProjectsSection";
-import ClientsSection from "@/components/client/sections/ClientsSection";
 import WhyChooseUsSection from "@/components/client/sections/WhyChooseUsSection";
 import ContactSection from "@/components/client/sections/ContactSection";
 import Footer from "@/components/client/sections/Footer";
