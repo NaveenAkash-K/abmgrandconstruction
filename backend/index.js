6455bb0ecd10be775ca58a9ae135402cd87cde58
Naveen Akash <naveen.akash0904@gmail.com>
2025-12-22 01:25:15 +0530

    Renamed server.js to index.js

--- backend/index.js
+++ backend/index.js
@@ -49,14 +49,14 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
+const index = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
+  index.close(() => process.exit(1));
 });
 
 // Handle uncaught exceptions
